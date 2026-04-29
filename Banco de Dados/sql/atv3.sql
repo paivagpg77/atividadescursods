6200d4d7f357357f7fbe8427b6c692080f001ffb
Gabriel  Paiva <paivag171@gmail.com>
2026-04-29 16:59:03 +0000

    Atividade 29/04/26

--- Banco de Dados/sql/atv3.sql
+++ Banco de Dados/sql/atv3.sql
@@ -17,20 +17,26 @@ CREATE TABLE Funcionario(
 	id_funcionario INT,
   	nome VARCHAR (50),
   	funcao VARCHAR (20),
+  	salario FLOAT,
   	CONSTRAINT pk_funcionario PRIMARY KEY (id_funcionario)
 );
-
+	
 #QUERY SQL
-
-INSERT INTO Funcionario (id_funcionario, nome ,funcao)
+INSERT INTO Funcionario (id_funcionario, nome ,funcao, salario)
 VALUES
-(1, 'João' ,'Caixa' ),
-(2, 'Maria', 'Repositora'),
-(3,'Marcos','Gerente');
+(1, 'João' ,'Caixa',1.104),
+(2, 'Maria', 'Repositora', 1.520),
+(3,'Marcos','Gerente' , 1.320),
+(4, 'Julia' , 'Empacotadora' , 1.340);
+SELECT funcao FROM Funcionario;
+SELECT nome , ROUND(salario * 1.10,3) AS salario_aumentado FROM Funcionario;
+SELECT nome , funcao FROM Funcionario
+WHERE (nome LIKE 'A__') OR (LOWER(nome) LIKE '%a');
+
 
 INSERT INTO Produto (id_produto, nome ,preco , qtd)
 VALUES
-(1, 'Arroz', 9.30 , 45),
-(2, 'Feijão' ,4.50, 45 );
-
-SELECT * FROM Funcionario;
+(1, 'Arroz', 9.39, 45),
+(2, 'Feijão' ,4.58, 45 );
+SELECT * FROM Produto;
+SELECT nome , ROUND(preco * 2,2) AS preco_aumentando FROM Produto;
