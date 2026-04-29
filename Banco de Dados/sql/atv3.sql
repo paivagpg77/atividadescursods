c67150bc7151d7cb4d40f1bc87120cb909e30a5a
Gabriel  Paiva <paivag171@gmail.com>
2026-04-29 17:52:32 +0000

    29/04

--- Banco de Dados/sql/atv3.sql
+++ Banco de Dados/sql/atv3.sql
@@ -30,13 +30,36 @@ VALUES
 (4, 'Julia' , 'Empacotadora' , 1.340);
 SELECT funcao FROM Funcionario;
 SELECT nome , ROUND(salario * 1.10,3) AS salario_aumentado FROM Funcionario;
+
 SELECT nome , funcao FROM Funcionario
 WHERE (nome LIKE 'A__') OR (LOWER(nome) LIKE '%a');
 
+SELECT nome , funcao FROM Funcionario
+WHERE nome IN ('João', 'Marcos');
 
-INSERT INTO Produto (id_produto, nome ,preco , qtd)
+
+
+INSERT INTO Produto (id_produto, nome, preco, qtd)
 VALUES
 (1, 'Arroz', 9.39, 45),
-(2, 'Feijão' ,4.58, 45 );
-SELECT * FROM Produto;
+(2, 'Feijão', 4.58, 45),
+(3, 'Macarrão', 2.99, 35), 
+(4, 'Farinha', 6.49, 56),
+(5, 'Ovo', 0.50, 100),
+(6, 'Oleo', 4.59, 78);
+
+SELECT COUNT(*) AS total_linhas 
+FROM Produto;
+SELECT MIN(preco) AS menor_preco
+FROM Produto;
+SELECT MAX(Preco) AS maior_preco
+FROM Produto;
+
+SELECT MIN(preco) AS menor_preco, MAX(preco) AS maior_preco
+FROM Produto;
+
+SELECT AVG(preco) as media_valor FROM Produto;
+
+SELECT SUM(qtd) as valor_total FROM Produto;
+SELECT SUM(qtd*preco) as valor_total FROM Produto;
 SELECT nome , ROUND(preco * 2,2) AS preco_aumentando FROM Produto;
